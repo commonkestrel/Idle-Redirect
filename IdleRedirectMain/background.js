5a9277919a2e4b282dcd24a28c0b3a856ffb9cec
Jibble330 <87543311+Jibble330@users.noreply.github.com>
2022-02-26 11:16:19 -0800

    Update background.js to manifest V3

--- IdleRedirectMain/background.js
+++ IdleRedirectMain/background.js
@@ -1,49 +1,53 @@
 let DefaultUrl = "https://www.google.com"
-let DefaultTimeout = 30
+let DefaultTimeout = 15
 chrome.storage.sync.get(["Url"], url => {
-  url = url["Url"]
-  if (typeof url != 'string') {
-  chrome.storage.sync.set({Url: DefaultUrl}, function() {
-    console.log('Url is set to ' + DefaultUrl);
-  });
+    url = url["Url"]
+    if (typeof url != 'string') {
+    chrome.storage.sync.set({Url: DefaultUrl}, function() {
+    });
 }
 });
-chrome.storage.sync.get(["Timeout"], timeout => {
-  timeout = timeout["Timeout"]
-  if (typeof timeout != 'string' && typeof timeout != 'integer') {
-  chrome.storage.sync.set({Timeout: DefaultTimeout}, function() {
-    console.log('Timeout is set to ' + DefaultTimeout);
-  });
-}
-});
-let lifeline;
 
-const IdleCheck = () => {
-  chrome.storage.sync.get(["Timeout"], timeout => {
-    timeout = parseInt(timeout["Timeout"]);
-    chrome.idle.queryState(
-        timeout, // in seconds
-        currentState => {
-          if (currentState != "active") {
-            chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-              var activeTab = tabs[0];
-              var activeTabUrl = activeTab.url;
-              console.log(activeTabUrl);
-              getUrl((value) => {
+chrome.storage.sync.get(["Timeout"], timeout => {
+    timeout = timeout["Timeout"]
+    if (typeof timeout != 'string' && typeof timeout != 'integer') {
+    chrome.storage.sync.set({Timeout: DefaultTimeout}, function() {
+        });
+    }
+});
+
+chrome.idle.setDetectionInterval(
+    15
+);
+
+const IdleCheck = currentState => {
+    if (currentState != "active") {
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+            var activeTab = tabs[0];
+            var activeTabUrl = activeTab.url;
+            getUrl((value) => {
                 let redirectUrl = value["Url"]
                 if (activeTabUrl.includes(redirectUrl) !== true) {
-                  chrome.tabs.update({url:redirectUrl});
-                  }
-              });
+                    chrome.tabs.update({url:redirectUrl});
+                    }
+                });
             });
-          } 
-        }
-      );
-    });
-  }
+        } 
+    }
 
 const getUrl = (callback) => {
-  chrome.storage.sync.get("Url", callback);
+    chrome.storage.sync.get("Url", callback);
 }
 
-setInterval(IdleCheck, 4000)
+chrome.storage.onChanged.addListener(() => {
+    chrome.storage.sync.get(["Timeout"], timeout => {
+        newTimeout = parseInt(timeout["Timeout"])
+        chrome.idle.setDetectionInterval(newTimeout);
+    });
+  });
+
+chrome.idle.onStateChanged.addListener(
+    newState => {
+        IdleCheck(newState)
+    }
+);
